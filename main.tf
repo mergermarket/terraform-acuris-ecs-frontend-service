bb05b0f7f1fafa93eb4bc421d4a9e399b321cc06
Neil Haggerty <neil.haggerty@mergermarket.com>
2019-02-26 15:38:42 +0000

    Changing to use no-secrets branch

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ module "taskdef" {
 }
 
 module "service_container_definition" {
-  source = "github.com/mergermarket/tf_ecs_container_definition.git"
+  source = "github.com/mergermarket/tf_ecs_container_definition.git?ref=no-secrets"
 
   name           = "${lookup(var.release, "component")}${var.name_suffix}"
   image          = "${lookup(var.release, "image_id")}"
